e1bfb27a194337f1f0c2e4ba3df00eb71368f20f
howhow0917 <tsaishunghow@gmail.com>
2026-08-09 16:34:49 +0800

    feat: add basic config setup

--- src/market_intelligence/config.py
+++ src/market_intelligence/config.py
@@ -1,1 +1,11 @@
+from dataclasses import dataclass
 
+
+@dataclass
+class Config:
+    project_name: str = "market-intelligence"
+    version: str = "0.1.0"
+    debug: bool = True
+
+
+config = Config()
